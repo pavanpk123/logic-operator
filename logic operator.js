73ebbc934d4fede800764980c2cb70038c02b379
pavan <Pavanpk123@gmail.com>
2022-08-17 15:19:41 +0530

    logic operator

---
+++ logic operator.js
@@ -0,0 +1,29 @@
+var a = 10
+var b = 20
+var c = 25
+var x = (a!=b) && (a<b)
+console.log(x)
+
+var a = 10
+var b = 20
+var c = 25
+var x = (a > b) || (a == b)
+console.log(x)
+
+var a = 10
+var b = 20
+var c = 25
+var x = (a < b) || (a == b)
+console.log(x)
+
+var a = 10
+var b = 20
+var c = 25
+var x = !(a < b)
+console.log(x)
+
+var a = 10
+var b = 20
+var c = 25
+var x = !(a > b)
+console.log(x)
